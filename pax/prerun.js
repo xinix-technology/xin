417926534b7a32b57f05f562a1772e767a629e31
Jafar Shadiq <jafar@xinix.co.id>
2013-10-29 16:19:40 +0700

    Use pax

--- pax/prerun.js
+++ pax/prerun.js
@@ -1,4 +1,5 @@
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-compass');
 };
