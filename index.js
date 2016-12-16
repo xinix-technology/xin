160e97b613bfb036ed7d9949ec59756d452a021b
Ganesha <reekoheek@gmail.com>
2016-12-16 16:50:02 +0700

    do not broken on duplicate xin

--- index.js
+++ index.js
@@ -2,27 +2,20 @@ import setup from './setup';
 import { get, put } from './repository';
 import { Component, base, define } from './component';
 
-const xin = window.xin = (() => {
-  if ('xin' in window) {
-    switch (typeof window.xin) {
-      case 'object':
-        setup.load(window.xin);
-        break;
-      case 'function':
-        console.warn('Duplicate xin library');
-        return window.xin;
-    }
-  }
+if (typeof window.xin === 'object') {
+  setup.load(window.xin);
+}
 
-  let xin = (id) => get(id);
+const xin = (id) => get(id);
 
-  xin.put = put;
-  xin.setup = setup;
-  xin.Component = Component;
-  xin.base = base;
-  xin.define = define;
+xin.put = put;
+xin.setup = setup;
+xin.Component = Component;
+xin.base = base;
+xin.define = define;
 
-  return xin;
-})();
+if (typeof window !== 'undefined') {
+  window.xin = xin;
+}
 
 export default xin;
