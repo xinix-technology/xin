40264a4d0b65536d8d6d002561eca30c3cdf2abc
Ganesha <reekoheek@gmail.com>
2017-03-08 08:41:42 +0700

    introduced component for in place of repeat

---
+++ components/repeat.js
@@ -0,0 +1,90 @@
+import T from 'template-binding';
+import xin from '../';
+import Row from './row';
+
+const FILTER_ALL = () => true;
+
+class Repeat extends xin.base('HTMLTemplateElement') {
+  get props () {
+    return {
+      items: {
+        type: Array,
+        observer: '_itemsChanged(items, filter)',
+      },
+
+      as: {
+        type: String,
+        value: 'item',
+      },
+
+      indexAs: {
+        type: String,
+        value: 'index',
+      },
+
+      filter: {
+        type: Function,
+        observer: '_itemsChanged(items, filter)',
+      },
+    };
+  }
+
+  created () {
+    super.created();
+
+    this.rows = [];
+  }
+
+  __initTemplate () {
+    T.prototype.__templateInitialize.call(this, null, this);
+  }
+
+  _itemsChanged (items, filter) {
+    this.debounce('_itemsChanged', () => {
+      let len = 0;
+
+      if (items && items.length) {
+        let filter = this.filter || FILTER_ALL;
+        items.filter(filter).forEach((item, index) => {
+          if (this.rows[index]) {
+            this.rows[index].update(item, index);
+          } else {
+            this.rows.push(new Row(this, this, item, index));
+          }
+
+          len++;
+        });
+      }
+
+      // move to detach
+      this.rows.splice(len).forEach(row => {
+        row.__templateUninitialize();
+      });
+    });
+  }
+
+  itemForElement (element) {
+    while (element && !element.__repeatModel) {
+      element = element.parentElement;
+    }
+    return element.__repeatModel.get(this.as);
+  }
+
+  indexForElement (element) {
+    while (element && !element.__repeatModel) {
+      element = element.parentElement;
+    }
+    return element.__repeatModel.get(this.indexAs);
+  }
+
+  modelForElement (element) {
+    while (element && !element.__repeatModel) {
+      element = element.parentElement;
+    }
+    return element.__repeatModel;
+  }
+}
+
+xin.define('xin-repeat', Repeat, { extends: 'template' });
+
+export default Repeat;
