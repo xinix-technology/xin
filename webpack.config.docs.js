2fe560a3892f4d0cbe3733df7d807196acdfb64e
Ganesha <reekoheek@gmail.com>
2018-03-22 19:47:08 +0700

    Move examples to docs

--- webpack.config.docs.js
+++ webpack.config.docs.js
@@ -4,12 +4,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = function (_, { mode = 'development' }) {
   return {
     mode,
-    context: path.join(__dirname, 'src/examples'),
+    context: path.join(__dirname, 'src/docs'),
     entry: {
       index: './index.js',
     },
     output: {
-      path: path.join(__dirname, 'examples'),
+      path: path.join(__dirname, 'docs'),
       filename: 'lib/[name].js',
     },
     // devtool: 'sourcemap',
