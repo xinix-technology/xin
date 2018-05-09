ab5a1c5f28a7e2da4dfd93cd96b1c4154c827ab6
Ganesha <reekoheek@gmail.com>
2018-05-09 19:32:54 +0700

    Fix number filter

--- component/filter.js
+++ component/filter.js
@@ -36,7 +36,7 @@ const registry = {
     return val;
   },
   string: val => String(val),
-  number: val => Number(val),
+  number: val => Number(val).toLocaleString(),
   boolean: val => Boolean(val),
   default: (val, defVal) => (val || defVal),
   upper: val => String.prototype.toUpperCase.call(val || ''),
